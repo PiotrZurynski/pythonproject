a3b3dd33b59d652127ac165c6effc76c3d40ffb3
PiotrZurynski <pewdiejanusz@gmail.com>
2024-10-03 19:31:12 +0200

    Added test objects

--- projekt.py
+++ projekt.py
@@ -1,1 +1,7 @@
-print(1+1)
+#Projekt Borderless Tourist
+list=["Paris, France","Shanghai, China","Los Angeles, USA","Sao Paulo, Brazil","Cairo, Egypt"]
+
+test_traveler=['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+
+   
